cdc88c302d320e3988528550f7aa2a07d7c492cc
priyansh0401 <priyansh0401@icloud.com>
2024-04-28 21:58:18 +0530

    a

--- frontend/script_login.js
+++ frontend/script_login.js
@@ -1,34 +1,38 @@
-const loginForm = document.querySelector('.form-wrapper.sign-in form');
-const signupForm = document.querySelector('.form-wrapper.sign-up form');
-const wrapper = document.querySelector('.wrapper');
-const signUpLink = document.querySelector('.signUp-link');
-const signInLink = document.querySelector('.signIn-link');
+const loginForm = document.querySelector(".form-wrapper.sign-in form");
+const signupForm = document.querySelector(".form-wrapper.sign-up form");
+const wrapper = document.querySelector(".wrapper");
+const signUpLink = document.querySelector(".signUp-link");
+const signInLink = document.querySelector(".signIn-link");
 
 // Switch to the sign-up form
-signUpLink.addEventListener('click', () => {
-  wrapper.classList.add('animate-signIn');
-  wrapper.classList.remove('animate-signUp');
+signUpLink.addEventListener("click", () => {
+  wrapper.classList.add("animate-signIn");
+  wrapper.classList.remove("animate-signUp");
 });
 
 // Switch to the login form
-signInLink.addEventListener('click', () => {
-  wrapper.classList.add('animate-signUp');
-  wrapper.classList.remove('animate-signIn');
+signInLink.addEventListener("click", () => {
+  wrapper.classList.add("animate-signUp");
+  wrapper.classList.remove("animate-signIn");
 });
 
 // Login form submission
-loginForm.addEventListener('submit', async (event) => {
+loginForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const loginEmailInput = document.querySelector('.form-wrapper.sign-in input[type="text"]');
-  const loginPasswordInput = document.querySelector('.form-wrapper.sign-in input[type="password"]');
+  const loginEmailInput = document.querySelector(
+    '.form-wrapper.sign-in input[type="text"]'
+  );
+  const loginPasswordInput = document.querySelector(
+    '.form-wrapper.sign-in input[type="password"]'
+  );
   const loginEmail = loginEmailInput.value;
   const loginPassword = loginPasswordInput.value;
 
   try {
-    const response = await fetch('backend-psi-lemon.vercel.app', {
-      method: 'POST',
+    const response = await fetch("https://backend-psi-lemon.vercel.app/login", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ email: loginEmail, password: loginPassword }),
     });
@@ -37,92 +41,99 @@ loginForm.addEventListener('submit', async (event) => {
 
     if (response.ok) {
       Toastify({
-        text: 'Login successful!',
+        text: "Login successful!",
         duration: 3000,
-        gravity: 'top',
-        position: 'right',
+        gravity: "top",
+        position: "right",
         style: {
-          background: 'linear-gradient(to right, #00b09b, #96c93d)',
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
         },
       }).showToast();
       loginForm.reset();
-      window.location.href = 'dashboard.html'; // Replace with your dashboard page URL
+      window.location.href = "dashboard.html"; // Replace with your dashboard page URL
     } else {
       Toastify({
-        text: data || 'Incorrect email or password. Please try again.',
+        text: data || "Incorrect email or password. Please try again.",
         duration: 3000,
-        gravity: 'top',
-        position: 'right',
+        gravity: "top",
+        position: "right",
         style: {
-          background: 'linear-gradient(to right, #ff5f6d, #ffc371)',
+          background: "linear-gradient(to right, #ff5f6d, #ffc371)",
         },
       }).showToast();
     }
   } catch (error) {
-    console.error('Error during login:', error);
+    console.error("Error during login:", error);
     Toastify({
-      text: 'Error logging in. Please try again.',
+      text: "Error logging in. Please try again.",
       duration: 3000,
-      gravity: 'top',
-      position: 'right',
+      gravity: "top",
+      position: "right",
       style: {
-        background: 'linear-gradient(to right, #ff5f6d, #ffc371)',
+        background: "linear-gradient(to right, #ff5f6d, #ffc371)",
       },
     }).showToast();
   }
 });
 
 // Sign-up form submission
-signupForm.addEventListener('submit', async (event) => {
+signupForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const signupEmailInput = document.querySelector('.form-wrapper.sign-up input[type="email"]');
-  const signupPasswordInput = document.querySelector('.form-wrapper.sign-up input[type="password"]');
+  const signupEmailInput = document.querySelector(
+    '.form-wrapper.sign-up input[type="email"]'
+  );
+  const signupPasswordInput = document.querySelector(
+    '.form-wrapper.sign-up input[type="password"]'
+  );
   const signupEmail = signupEmailInput.value;
   const signupPassword = signupPasswordInput.value;
 
   // console.log()
   try {
-    const response = await fetch('backend-psi-lemon.vercel.app', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ email: signupEmail, password: signupPassword }),
-    });
+    const response = await fetch(
+      "https://backend-psi-lemon.vercel.app/signup",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: signupEmail, password: signupPassword }),
+      }
+    );
 
     const data = await response.text();
 
     if (response.ok) {
       Toastify({
-        text: 'Sign up successful! Please login.',
+        text: "Sign up successful! Please login.",
         duration: 3000,
-        gravity: 'top',
-        position: 'right',
+        gravity: "top",
+        position: "right",
         style: {
-          background: 'linear-gradient(to right, #00b09b, #96c93d)',
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
         },
       }).showToast();
       signupForm.reset();
     } else {
       Toastify({
-        text: data || 'User already exists. Please login instead.',
+        text: data || "User already exists. Please login instead.",
         duration: 3000,
-        gravity: 'top',
-        position: 'right',
+        gravity: "top",
+        position: "right",
         style: {
-          background: 'linear-gradient(to right, #ff5f6d, #ffc371)',
+          background: "linear-gradient(to right, #ff5f6d, #ffc371)",
         },
       }).showToast();
     }
   } catch (error) {
-    console.error('Error during signup:', error);
+    console.error("Error during signup:", error);
     Toastify({
-      text: 'Error signing up. Please try again.',
+      text: "Error signing up. Please try again.",
       duration: 3000,
-      gravity: 'top',
-      position: 'right',
+      gravity: "top",
+      position: "right",
       style: {
-        background: 'linear-gradient(to right, #ff5f6d, #ffc371)',
+        background: "linear-gradient(to right, #ff5f6d, #ffc371)",
       },
     }).showToast();
   }
